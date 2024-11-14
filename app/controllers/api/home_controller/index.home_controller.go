d664681f1015b334803303199402b782feda11e4
Untara Vivi Chahya <vyvy1777@gmail.com>
2024-11-14 16:45:24 +0700

    check connect db

--- app/controllers/api/home_controller/index.home_controller.go
+++ app/controllers/api/home_controller/index.home_controller.go
@@ -1,6 +1,8 @@
 package home_controller
 
 import (
+	"gin-gorm/app/model"
+	"gin-gorm/database"
 	"strconv"
 	"time"
 
@@ -22,6 +24,19 @@ func VersionCheck(ctx *gin.Context) {
 	app_version := ctx.DefaultQuery("app_version", "1")
 
 	app_version_int, _ := strconv.Atoi(app_version)
+
+	var data_karyawan model.Karyawan
+	err_karyawan := database.DB.Table("karyawan").
+		Where("karyawan_email = ?", "tries1@mail.com").
+		First(&data_karyawan).Error
+
+	if err_karyawan != nil {
+		ctx.JSON(202, gin.H{
+			"status": false,
+			"text":   "ERR DB karyawan",
+		})
+		return
+	}
 
 	ctx.JSON(200, gin.H{
 		"status": true,
