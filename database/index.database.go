b15d45460c78bb1695257a12e8ef58cb4c1c3044
Untara Vivi Chahya <vyvy1777@gmail.com>
2024-11-14 17:19:26 +0700

    v1

--- database/index.database.go
+++ database/index.database.go
@@ -14,11 +14,11 @@ var DB *gorm.DB
 
 func ConnectDatabase() {
 	var errConnection error
-
+	fmt.Println(db_config.DB_DRIVER)
 	if db_config.DB_DRIVER == "mysql" {
 		dsnMysql := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s", db_config.DB_USER, db_config.DB_PASSWORD, db_config.DB_HOST, db_config.DB_PORT, db_config.DB_NAME)
-		DB, errConnection = gorm.Open(mysql.Open(dsnMysql), &gorm.Config{})
 		fmt.Println(dsnMysql)
+		DB, errConnection = gorm.Open(mysql.Open(dsnMysql), &gorm.Config{})
 	}
 
 	if errConnection != nil {
